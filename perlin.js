4042b0bf5ef45d8c2f8e6a4c4b87011454a82fa3
Curtis Ullerich <curtisullerich@gmail.com>
2022-04-11 20:24:39 -0700

    Draw a row of circles

--- perlin.js
+++ perlin.js
@@ -1,7 +1,18 @@
 function setup() {
   createCanvas(400, 400);
+  frameRate(30);
 }
 
 function draw() {
-  background(220);
+  background('white');
+  var y = height/2;
+  for (var i = 0; i < width; i++) {
+    var x = i;
+    drawPoint(x, y);
+  }
+}
+
+function drawPoint(x, y) {
+  fill('green');
+  circle(x, y, 100);
 }
